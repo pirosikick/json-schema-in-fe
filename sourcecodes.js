97ab573621bdae0104bb33b4e0405a7bb1db82a3
pirosikick <pirosikick@gmail.com>
2017-02-21 18:19:15 +0900

    Modified

--- sourcecodes.js
+++ sourcecodes.js
@@ -41,3 +41,27 @@
     }
   }
 }
+//====================
+{
+  "$schema": "http://json-schema.org/draft-04/hyper-schema",
+  "type": "object",
+  "definitions": { ... },
+  "properties": { ... },
+  "links": [{
+    "title": "ユーザ取得",
+    "description": "idに紐づくユーザを返す",
+    "href": "/user/:id",
+    "method": "GET",
+    "rel": "self"
+  }, {
+    "title": "ユーザ新規登録",
+    "description": "idに紐づくユーザを返す",
+    "href": "/user",
+    "method": "POST",
+    "rel": "create",
+    "schema": {
+      "type": "object",
+      "properties": { ... }
+    }
+  }]
+}
