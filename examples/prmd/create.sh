95331ec6f5457026711571b2ddd6cfbe08c9d477
pirosikick <pirosikick@gmail.com>
2017-02-23 23:30:51 +0900

    Modified

--- examples/prmd/create.sh
+++ examples/prmd/create.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# vim: set background=light nolist nonumber:
 
-# ./schemata/*.yamlとmeta.jsonを結合し、
-# schema.jsonを生成する
+# 雛形を生成
+# - schemata以下にリソース毎にファイルを生成
+# - JSONとYAMLで書くことが出来る（--yamlでYAML）
+prmd init --yaml user > schemata/user.yaml
+
+# JSON Hyper-Schema（schema.json）の生成
+# - ./schemata/*.{yaml,json}とmeta.jsonを結合して生成する
 prmd combine -m meta.json ./schemata > schema.json
 
-prmd doc schema.json > README.md
+# Markdownの生成
+# - prmd combineで生成したschema.jsonからREADME.mdを生成
+# - OVERVIEW.mdを出力ファイルの先頭に挿入
+prmd doc --prepend OVERVIEW.md schema.json > README.md
